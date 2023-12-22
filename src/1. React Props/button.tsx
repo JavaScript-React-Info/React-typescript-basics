6e3711b291f9bcd5b124985dc19800735adac5b6
shubham V <Shubham.V@cerner.com>
2023-12-22 12:54:49 +0530

    fecthing data

--- src/1. React Props/button.tsx
+++ src/1. React Props/button.tsx
@@ -8,6 +8,18 @@ type ButtonProps = {
 }
 
 const Button = () => {
+
+  useEffect(()=>{
+      fetch("https://jsonplcaeholder.typicode.com/todos/1")
+      .then((response)=> response.json())
+      .then((data: unknown)=>{ // instead of any it is better to have unknown
+        // this will prevent having data.name.toUpperCase
+        // we need to define schema for this
+
+        // test with ZOD to have schema
+      })
+  },[])
+
   return (
     <button>
       Click Me
