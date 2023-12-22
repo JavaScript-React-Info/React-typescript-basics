de5cb81993d9fdf717c941511188b9829c37f2db
shubham V <Shubham.V@cerner.com>
2023-12-22 12:19:25 +0530

    useState typescript-----------------

--- src/1. React Props/button.tsx
+++ src/1. React Props/button.tsx
@@ -1,13 +1,38 @@
 
-import React, { Component, ComponentProps } from 'react'
+import React, { Component, ComponentProps, useState } from 'react'
+
+type User = {
+  name: string,
+  age: number
+}
+
+type Posts ={
+  name: string,
+  age: number
+}[]
 
 
 const Button = () => {
+  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState<number>(0) //<number> is not neccessary because typescript will infer this by default
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => console.log("clicked");
-  
+   const [text, setText] = useState("Click me")
+  //const [text, setText] = useState<string>("Click me") //<string> is not neccessary because typescript will infer this 
+
+  const [isPrimary, setPrimary]= useState(true);
+  //const [isPrimary, setPrimary]= useState<boolean>(true)//<boolean> is not neccessary because typescript will infer this 
+
+  const [user, setUser]= useState<User|null>(null);
+
+  const [posts, setPosts] = useState<Posts>([])
+
+
+  const name = user?.name;
+
+  const postName = posts[0].name
+
   return (
-    <button onClick={handleClick}>
+    <button >
       Click me
     </button>
   )
