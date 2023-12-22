604efb33a4760e6f1b38bc706e0cd906aafbb662
shubham V <Shubham.V@cerner.com>
2023-12-22 12:23:04 +0530

    useRef----------------------------

--- src/1. React Props/button.tsx
+++ src/1. React Props/button.tsx
@@ -1,38 +1,22 @@
 
-import React, { Component, ComponentProps, useState } from 'react'
-
-type User = {
-  name: string,
-  age: number
-}
-
-type Posts ={
-  name: string,
-  age: number
-}[]
+import React, { Component, ComponentProps, useEffect, useRef, useState } from 'react'
 
 
 const Button = () => {
-  const [count, setCount] = useState(0)
-  //const [count, setCount] = useState<number>(0) //<number> is not neccessary because typescript will infer this by default
 
-   const [text, setText] = useState("Click me")
-  //const [text, setText] = useState<string>("Click me") //<string> is not neccessary because typescript will infer this 
+  // useEffect doesnt need any type
+  // useEffect(() => {
+  //   return()=>{
+  //   }
+  // },[])
 
-  const [isPrimary, setPrimary]= useState(true);
-  //const [isPrimary, setPrimary]= useState<boolean>(true)//<boolean> is not neccessary because typescript will infer this 
-
-  const [user, setUser]= useState<User|null>(null);
-
-  const [posts, setPosts] = useState<Posts>([])
-
-
-  const name = user?.name;
-
-  const postName = posts[0].name
+  const ref = useRef<HTMLButtonElement>(null);
+  //Element
+  //HTMLButtonElement
+  //HTMLElement
 
   return (
-    <button >
+    <button ref={ref}>
       Click me
     </button>
   )
