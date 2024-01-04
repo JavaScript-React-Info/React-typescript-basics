2b88668493ae63de9bb76fd905a2e5d3c9702cb8
shubham V <Shubham.V@cerner.com>
2024-01-04 15:51:21 +0530

    commit basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
 
-import React from 'react'
+import React, { useCallback } from 'react'
+
+type HeadingProps = {
+  title: string
+}
+
+const Heading = (props: HeadingProps)=> (
+  <h2>{props.title}</h2>
+)
+
+type BoxProps = {
+  children: React.ReactNode
+}
+
+// const Box = (props: BoxProps) => (
+//   <div>
+//     {props.children}
+//   </div>
+// )
+const Box : React.FunctionComponent<{children: React.ReactNode}>= (props) => (
+  <div>
+    {props.children}
+  </div>
+)
+
+const List: React.FunctionComponent<{items: string[], onClick? : (item :string)=> void}> = ({items, onClick})=>(
+  <ul>
+    {
+      items.map((item, index)=>(
+        <li key={index} onClick={() => onClick?.(item)}>{item}</li>
+      ))
+    }
+  </ul>
+)
 
 const App = () => {
+  const onListClick = useCallback((item: string)=>{
+    alert(item)
+  }, [])
+
   return (
-    <div>App</div>
+    <div>
+      <Heading title="Introduction"/>
+      <Box>
+        Hello there
+      </Box>
+      <List items={["one", "two", "three"]} onClick={onListClick}/>
+    </div>
   )
 }
 
