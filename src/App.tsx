0772b024a7079c44e7131b8009ad3cffac3c54ac
shubham V <Shubham.V@cerner.com>
2024-01-04 12:22:00 +0530

    reset initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import Container from "./components/Container"
-import Box from "./components/context/Box"
-import { ThemContextProvider } from "./components/context/ThemeContext"
-import User from "./components/context/User"
-import { UserContextProvider } from "./components/context/UserContext"
-import DomRef from "./components/ref/DomRef"
-import Counter from "./state/Counter"
-import LoggedIn from "./state/LoggedIn"
 
+import React from 'react'
 
 const App = () => {
   return (
-    <div>
-     <DomRef/>
-    </div>
+    <div>App</div>
   )
 }
 
